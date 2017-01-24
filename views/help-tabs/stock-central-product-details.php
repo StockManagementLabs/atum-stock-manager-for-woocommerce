59380c07ceb0b9e2269a22f8811127ec08b65716
Salva Machi <salvamb@sispixels.com>
2017-01-24 12:31:10 +0100

    Help tab changes

--- views/help-tabs/stock-central-product-details.php
+++ views/help-tabs/stock-central-product-details.php
@@ -17,11 +17,7 @@ defined( 'ABSPATH' ) or die;
 	</thead>
 	<tbody>
 		<tr>
-			<td><strong><?php _e( 'Checkbox', ATUM_TEXT_DOMAIN ) ?></strong></td>
-			<td><?php printf( __( "Select an item you would like to work with, amend or filter. We introduce new filters and functions often, however, if you think we should add one urgently, don't hesitate to let us know %shere%s.", ATUM_TEXT_DOMAIN ), '<a href="http://www.stockmanagementlabs.com/feature-request" target="_blank">', '</a>' ) ?></td>
-		</tr>
-		<tr>
-			<td><span class="dashicons dashicons-format-image"></span></td>
+			<td><span class="dashicons dashicons-format-image" title="<?php _e('Thumbnail', ATUM_TEXT_DOMAIN) ?>"></span></td>
 			<td><?php _e( 'Product small image preview.', ATUM_TEXT_DOMAIN ) ?></td>
 		</tr>
 		<tr>
@@ -37,7 +33,7 @@ defined( 'ABSPATH' ) or die;
 			<td><?php _e( "A WooCommerce Product ID is sometimes needed when using shortcodes, widgets and links. ATUM's stock central page will display the appropriate ID of the product in this column.", ATUM_TEXT_DOMAIN ) ?></td>
 		</tr>
 		<tr>
-			<td><strong><?php _e( 'Product Type', ATUM_TEXT_DOMAIN ) ?></strong></td>
+			<td><span class="wc-type" title="<?php _e( 'Product Type', ATUM_TEXT_DOMAIN ) ?>"></span></td>
 			<td><?php _e( 'This column displays the classification of individual products in WooCommerce. We specify product types by icons with a tooltip on hover.', ATUM_TEXT_DOMAIN ) ?></td>
 		</tr>
 	</tbody>
