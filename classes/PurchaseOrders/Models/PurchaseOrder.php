f3c2c812285b56e57672e44852f642cf5570778d
Salva Machi <salvamb@sispixels.com>
2017-10-31 17:45:33 +0100

    Bug fix in PO with no supplier specified

--- classes/PurchaseOrders/Models/PurchaseOrder.php
+++ classes/PurchaseOrders/Models/PurchaseOrder.php
@@ -109,7 +109,10 @@ class PurchaseOrder extends AtumOrderModel {
 	public function product_search_message($po) {
 
 		$supplier = $po->get_supplier();
-		echo '<em class="alert"><i class="dashicons dashicons-info"></i> ' . sprintf( __("Only products linked to '%s' supplier can be searched."), $supplier->post_title ) . '</em>';
+
+		if ($supplier) {
+			echo '<em class="alert"><i class="dashicons dashicons-info"></i> ' . sprintf( __( "Only products linked to '%s' supplier can be searched." ), $supplier->post_title ) . '</em>';
+		}
 	}
 
 	//---------
