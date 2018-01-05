a8ff10148094aa8f52a1623fb91cbe6e2c631b87
Salva Machi <salvamb@sispixels.com>
2018-01-05 17:17:02 +0100

    Added suppliers info to the Stock Central’s help tab

--- views/help-tabs/stock-central/product-details.php
+++ views/help-tabs/stock-central/product-details.php
@@ -25,8 +25,16 @@ defined( 'ABSPATH' ) or die;
 			<td><?php _e( 'The first twenty characters of the product name. Hover your mouse over the name to see the full content.', ATUM_TEXT_DOMAIN ) ?></td>
 		</tr>
 		<tr>
+			<td><strong><?php _e( 'Supplier', ATUM_TEXT_DOMAIN ) ?></strong></td>
+			<td><?php _e( 'This is the name of the suppliers that supplies the products for your store.', ATUM_TEXT_DOMAIN ) ?></td>
+		</tr>
+		<tr>
 			<td><strong><?php _e( 'SKU', ATUM_TEXT_DOMAIN ) ?></strong></td>
 			<td><?php _e( "An SKU, or Stock Keeping Unit, is a code assigned to a product by the store admin to identify the price, product options and manufacturer of the merchandise. An SKU is used to track inventory in your retail store. They are critical in helping you maintain a profitable retail business. We recommend the introduction of SKUs in your store to take the full advantage of ATUM's features.", ATUM_TEXT_DOMAIN ) ?></td>
+		</tr>
+		<tr>
+			<td><strong><?php _e( 'Supplier SKU', ATUM_TEXT_DOMAIN ) ?></strong></td>
+			<td><?php _e( "The stock keeping unit code of the product within your supplier's product list.", ATUM_TEXT_DOMAIN ) ?></td>
 		</tr>
 		<tr>
 			<td><strong><?php _e( 'ID', ATUM_TEXT_DOMAIN ) ?></strong></td>
