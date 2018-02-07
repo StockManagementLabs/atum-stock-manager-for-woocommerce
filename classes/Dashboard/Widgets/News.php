ac53ea074fe7f5029b9eaae127737c5742098615
Salva Machi <salvamb@sispixels.com>
2018-02-07 11:56:33 +0100

    News widget for ATUM dashboard

--- classes/Dashboard/Widgets/News.php
+++ classes/Dashboard/Widgets/News.php
@@ -15,6 +15,7 @@ namespace Atum\Dashboard\Widgets;
 defined( 'ABSPATH' ) or die;
 
 use Atum\Components\AtumWidget;
+use Atum\Inc\Helpers;
 
 
 class News extends AtumWidget {
@@ -46,7 +47,35 @@ class News extends AtumWidget {
 	 * @inheritDoc
 	 */
 	public function render() {
-		include ATUM_PATH . 'views/widgets/news.php';
+
+		add_filter( 'wp_feed_cache_transient_lifetime', array($this, 'limit_feed_cache') );
+		$sml_feed = fetch_feed( 'https://www.stockmanagementlabs.com/feed/' );
+		remove_filter( 'wp_feed_cache_transient_lifetime', array($this, 'limit_feed_cache') );
+
+		$max_items = 0;
+		$rss_items = array();
+
+		if ( ! is_wp_error($sml_feed) ) {
+			// Figure out how many total items there are, but limit it to 10
+			$max_items = $sml_feed->get_item_quantity( 10 );
+
+			// Build an array of all the items, starting with element 0 (first element)
+			$rss_items = $sml_feed->get_items( 0, $max_items );
+		}
+
+		Helpers::load_view( 'widgets/news', compact('max_items', 'rss_items') );
+
+	}
+
+	/**
+	 * Change the feed cache limit to 7200 seconds (2 hours)
+	 *
+	 * @since 1.3.9
+	 *
+	 * @return int
+	 */
+	public function limit_feed_cache() {
+		return 7200;
 	}
 
 	/**
--- views/widgets/news.php
+++ views/widgets/news.php
@@ -5,5 +5,50 @@
  * @since 1.3.9
  */
 ?>
+<div class="news-widget" data-widget="news">
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequatur consequuntur eligendi enim error maiores, provident quidem quisquam reiciendis similique unde voluptatibus. Animi autem consectetur exercitationem molestiae officia, tempora?</p>
+	<?php if ($max_items > 0) : ?>
+
+		<div class="scrollbar-inner">
+			<?php foreach ( $rss_items as $item ) : ?>
+				<article>
+
+					<a href="<?php echo esc_url( $item->get_permalink() ) ?>" class="post-thumb">
+						<?php
+						$attachment = $item->get_enclosure();
+
+						if ( ! empty($attachment) && ! empty($attachment->link) ): ?>
+							<div class="thumb">
+								<div style="background-image: url('<?php echo $attachment->link ?>')"></div>
+							</div>
+						<?php endif ?>
+					</a>
+
+					<div class="post-details">
+
+						<a href="<?php echo esc_url( $item->get_permalink() ) ?>" class="post-title" title="<?php echo esc_attr( $item->get_title() ) ?>" target="_blank"><?php echo $item->get_title() ?></a>
+
+						<div class="post-meta">
+							<?php
+							echo $item->get_author()->name;
+
+							$timeAgo = new \Westsworld\TimeAgo();
+							echo ' · ' . $timeAgo->inWords( $item->get_date() );
+							?>
+						</div>
+
+						<div class="post-excerpt">
+							<?php echo $item->get_content() ?>
+						</div>
+
+					</div>
+
+				</article>
+			<?php endforeach; ?>
+		</div>
+
+	<?php else: ?>
+		<p class="error"><?php _e('The Stock Management Labs news could not be loaded. Please try again later.', ATUM_TEXT_DOMAIN) ?></p>
+	<?php endif ?>
+
+</div>
