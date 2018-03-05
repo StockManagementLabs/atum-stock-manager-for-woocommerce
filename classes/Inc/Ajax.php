6ce26ded03347ab550dfcc0dd2f8dc71ed4343da
Salva Machi <salvamb@berebel.io>
2018-03-05 11:54:33 +0100

    Fixed importing variation products from WC order to Inventory Logs

--- classes/Inc/Ajax.php
+++ classes/Inc/Ajax.php
@@ -1562,7 +1562,7 @@ final class Ajax {
 
 		check_ajax_referer( 'import-order-items', 'security' );
 
-		if ( ! current_user_can( 'edit_shop_orders' ) ) {
+		if ( ! current_user_can( 'edit_shop_orders' ) || ! AtumCapabilities::current_user_can('edit_inventory_log') ) {
 			wp_die( -1 );
 		}
 
@@ -1590,7 +1590,7 @@ final class Ajax {
 						foreach ($items as $item) {
 
 							if ( is_a($item, '\WC_Order_Item_Product') ) {
-								$log_item = $atum_order->add_product( wc_get_product( $item->get_product_id() ), $item->get_quantity() );
+								$log_item = $atum_order->add_product( $item->get_product(), $item->get_quantity() );
 							}
 							elseif ( is_a($item, '\WC_Order_Item_Fee') ) {
 								$log_item = $atum_order->add_fee($item);
