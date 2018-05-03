655b32bc9ce599e7fdd28a08c448aff48d2f756b
Salva Machi <salvamb@berebel.io>
2018-05-03 13:56:22 +0200

    CSS fixes

--- views/meta-boxes/product-data/atum-variation-panel.php
+++ views/meta-boxes/product-data/atum-variation-panel.php
@@ -13,17 +13,21 @@ use Atum\Inc\Helpers;
 <div class="atum-data-panel">
 	<h3 class="atum-section-title"><?php _e('ATUM Inventory', ATUM_TEXT_DOMAIN) ?></h3>
 
-	<?php
-	woocommerce_wp_checkbox( array(
-		'id'          => Globals::ATUM_CONTROL_STOCK_KEY . '_' . $loop,
-		'name'        => "variation_atum_tab[" . Globals::ATUM_CONTROL_STOCK_KEY . "][$loop]",
-		'value'       => Helpers::get_atum_control_status( $variation->ID ),
-		'class'       => 'js-switch',
-		'label'       => __( 'ATUM Control Switch', ATUM_TEXT_DOMAIN ),
-		'description' => __( "Turn the switch ON or OFF to allow the ATUM plugin to include this product in its lists, counters and statistics.", ATUM_TEXT_DOMAIN ),
-		'desc_tip'    => TRUE
-	) );
+	<div class="options_group">
+		<?php
+		woocommerce_wp_checkbox( array(
+			'id'          => Globals::ATUM_CONTROL_STOCK_KEY . '_' . $loop,
+			'name'        => "variation_atum_tab[" . Globals::ATUM_CONTROL_STOCK_KEY . "][$loop]",
+			'value'       => Helpers::get_atum_control_status( $variation->ID ),
+			'class'       => 'js-switch',
+			'label'       => __( 'ATUM Control Switch', ATUM_TEXT_DOMAIN ),
+			'description' => __( "Turn the switch ON or OFF to allow the ATUM plugin to include this product in its lists, counters and statistics.", ATUM_TEXT_DOMAIN ),
+			'desc_tip'    => TRUE
+		) );
+		?>
+	</div>
 
+	<?php
 	// Allow other fields to be added to the ATUM panel
 	do_action('atum/after_variation_product_data_panel', $loop, $variation_data, $variation); ?>
 </div>
