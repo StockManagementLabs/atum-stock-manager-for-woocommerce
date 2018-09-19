ea50de0fd550ab32305068f9451158acec43bba4
José Andrés Piera <japiera@sispixels.com>
2018-09-19 07:54:38 +0200

    Added 'Not found' text to ATUM Locations

--- classes/Inc/Main.php
+++ classes/Inc/Main.php
@@ -140,6 +140,7 @@ class Main {
 			'add_new_item'      => __( 'Add new location', ATUM_TEXT_DOMAIN ),
 			'new_item_name'     => __( 'New location name', ATUM_TEXT_DOMAIN ),
 			'menu_name'         => __( 'ATUM Locations', ATUM_TEXT_DOMAIN ),
+            'not_found'         => __( 'No locations found', ATUM_TEXT_DOMAIN ),
 		);
 
 		$args = apply_filters( 'atum/location_taxonomy_args', array(
