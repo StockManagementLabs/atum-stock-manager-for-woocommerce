e8a2e33d352f0ba255aae76780933f8b291c05e6
Julián Gallego <juliang@berebel.io>
2018-10-08 13:33:06 +0200

    Fix style bug

--- views/settings-page.php
+++ views/settings-page.php
@@ -88,7 +88,7 @@ $menu_theme = get_user_meta( get_current_user_id(), 'menu_settings_theme', TRUE 
 					foreach ( array_keys($active_sections) as $active_section_key =>$active_section ):
 
 						// Check if is last section
-						$last_section = count( $active_sections ) - 1 === $active_section_key && 1 !== count( $active_sections ) ? true : false;
+						$last_section = count( $active_sections ) - 1 === $active_section_key ? true : false;
 
 						// This prints out all hidden setting fields
 						settings_fields( ATUM_PREFIX . "setting_$active_section" );
@@ -102,7 +102,7 @@ $menu_theme = get_user_meta( get_current_user_id(), 'menu_settings_theme', TRUE 
 						foreach ( (array) $wp_settings_sections[$page] as $section ): ?>
 
 							<div id="<?php echo $section['id'] ?>" class="settings-section" data-section="<?php echo str_replace([ATUM_PREFIX, 'setting_'], '', $section['id']) ?>">
-								<?php if ( ! $last_section ) : ?>
+								<?php if ( ! $last_section || 1 === count( $active_sections ) ) : ?>
 									<?php $menu_theme = get_user_meta( get_current_user_id(), 'menu_settings_theme', TRUE ); ?>
 									<div class="section-general-title <?php echo isset( $menu_theme ) && 'light' === $menu_theme ? 'section-general-title-light' : '' ?>">
 										<?php
