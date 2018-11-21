11b2cdac74883ed1e2a0b34e0ddb4aa6a0f77ade
Julián <juliang@berebel.io>
2018-11-21 13:54:13 +0100

    Fix bug

--- classes/Components/AtumListTables/AtumListTable.php
+++ classes/Components/AtumListTables/AtumListTable.php
@@ -2843,7 +2843,7 @@ abstract class AtumListTable extends \WP_List_Table {
 		?>
 		<div class="tablenav <?php echo esc_attr( $which ); ?>">
 
-			<div id="scroll-filters_container" class="filters-containe-box <?php echo 'top' === $which && isset( $this->_pagination_args ) && $this->_pagination_args['total_pages'] <= 1 ? 'not-pagination' : ''; ?> ">
+			<div id="scroll-filters_container" class="filters-container-box<?php echo 'top' === $which && isset( $this->_pagination_args ) && $this->_pagination_args['total_pages'] <= 1 ? ' not-pagination' : ''; ?>">
 				<div id="filters_container" class="<?php echo 'top' === $which ? 'nav-with-scroll-effect' : ''; ?> dragscroll">
 					<?php if ( ! empty( $this->get_bulk_actions() ) ) : ?>
 						<div class="alignleft actions bulkactions">
