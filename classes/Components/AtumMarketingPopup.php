72094eb2fa2edb04e5eb7b242456de78c0757cf5
Julián <juliang@berebel.io>
2019-01-09 08:48:21 +0100

    Change stockmanagementlabs url local to live

--- classes/Components/AtumMarketingPopup.php
+++ classes/Components/AtumMarketingPopup.php
@@ -64,7 +64,7 @@ class AtumMarketingPopup {
 	/**
 	 * The ATUM's addons store URL
 	 */
-	const MARKETING_POPUP_STORE_URL = 'http://stockmanagementlabs.loc/';
+	const MARKETING_POPUP_STORE_URL = 'https://stockmanagementlabs.com/';
 
 	/**
 	 * The ATUM's addons API endpoint
