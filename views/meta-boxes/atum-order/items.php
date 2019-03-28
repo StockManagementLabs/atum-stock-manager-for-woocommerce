9d60a717aabe77e1bb4dd7f965c26bb74e1df7e6
José Andrés Piera <japiera@sispixels.com>
2019-03-28 12:13:37 +0100

    Bug fix: Taxes non deleted in Atum Orders

--- views/meta-boxes/atum-order/items.php
+++ views/meta-boxes/atum-order/items.php
@@ -28,6 +28,7 @@ if ( wc_tax_enabled() ) {
 $currency  = $atum_order->get_currency();
 $post_type = get_post_type_object( get_post_type( $atum_order->get_id() ) );
 
+// ! wp_doing_ajax allow add taxes in still non-created order items.
 $add_blocker = ( ! $atum_order->get_status() && ! wp_doing_ajax() ) || ( PurchaseOrders::get_post_type() === $post_type->name && empty( $line_items ) && ! $atum_order->has_multiple_suppliers() );
 ?>
 
