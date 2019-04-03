425d25e52740d9ba87b3b84c2be73dca0daa53ea
José Andrés Piera <japiera@sispixels.com>
2019-04-03 16:10:59 +0200

    Added option to only add the tooltip if the container is smaller than the content

--- assets/js/src/components/_tooltip.ts
+++ assets/js/src/components/_tooltip.ts
@@ -11,12 +11,12 @@ export default class Tooltip {
 		this.addTooltips();
 		
 	}
-	
-	/**
-	 * Enable tooltips
-	 *
-	 * @param jQuery $wrapper   Optional. The wrapper where the elements with tooltips are contained,
-	 */
+
+    /**
+     * Enable tooltips
+     *
+     * @param $wrapper JQuery Optional. The wrapper where the elements with tooltips are contained,
+     */
 	addTooltips($wrapper?: JQuery) {
 		
 		if (!$wrapper) {
@@ -26,30 +26,47 @@ export default class Tooltip {
 		$wrapper.find('.tips, .atum-tooltip').each( (index: number, elem: Element) => {
 			
 			const $tipEl: any = $(elem);
-			
-			$tipEl.tooltip({
-				html     : true,
-				title    : $tipEl.data('tip') || $tipEl.attr('title'),
-				container: 'body',
-			});
-			
+			let doTooltip: boolean = false;
+
+			// Only if the container is smaller than the content.
+			if ( typeof $tipEl.data('tooltip') !== 'undefined' && $tipEl.data('tooltip') === 'overflown' ) {
+
+			    console.log($tipEl[0].scrollWidth);
+                console.log($tipEl.innerWidth());
+
+			    if ( $tipEl[0].scrollWidth > $tipEl.innerWidth()) {
+			        doTooltip = true;
+                }
+            }
+			else {
+			    doTooltip = true;
+            }
+
+			if (doTooltip) {
+                $tipEl.tooltip({
+                    html     : true,
+                    title    : $tipEl.data('tip') || $tipEl.attr('title'),
+                    container: 'body',
+                });
+
+            }
 		});
 		
 	}
-	
-	/**
-	 * Destroy all the tooltips
-	 *
-	 * @param jQuery $wrapper   Optional. The wrapper where the elements with tooltips are contained
-	 */
-	destroyTooltips($wrapper?: JQuery) {
-		
-		if (!$wrapper) {
-			$wrapper = $('body');
-		}
-		
-		(<any>$wrapper.find('.tips, .atum-tooltip')).tooltip('destroy');
-		
-	}
+
+    /**
+     * Destroy all the tooltips
+     *
+     * @param $wrapper JQuery Optional. The wrapper where the elements with tooltips are contained
+     */
+    destroyTooltips($wrapper?: JQuery) {
+
+        if (!$wrapper) {
+            $wrapper = $('body');
+        }
+
+        (<any>$wrapper.find('.tips, .atum-tooltip')).tooltip('destroy');
+
+    }
 	
 }
