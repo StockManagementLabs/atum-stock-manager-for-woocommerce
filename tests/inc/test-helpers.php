2c541a24a98dfd443a02d687769b5f58328f4159
Miguel Angel Santos <miguelangel@berebel.io>
2019-10-01 11:12:53 +0200

    Tests globals

---
+++ tests/inc/test-helpers.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class HelpersTest
+ *
+ * @package Atum_Stock_Manager_For_Woocommerce
+ */
+
+use Atum\Inc\Helpers;
+use TestHelpers\TestHelpers;
+
+
+/**
+ * Sample test case.
+ */
+class HelpersTest extends PHPUnit_Framework_TestCase { //WP_UnitTestCase {
+
+	public function test_get_term_ids_by_slug() {
+	}
+
+}
