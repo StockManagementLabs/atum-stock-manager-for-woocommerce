127b5c6bfdb78eb47a33c45726bf163833d51e99
Salva Machí <salvamb@berebel.io>
2019-10-04 09:32:28 +0200

    All the POs’ CRUD methods working now

--- classes/Api/Controllers/V3/PurchaseOrdersController.php
+++ classes/Api/Controllers/V3/PurchaseOrdersController.php
@@ -17,6 +17,9 @@ defined( 'ABSPATH' ) || exit;
 
 use Atum\Components\AtumOrders\AtumOrderPostType;
 use Atum\Inc\Helpers;
+use Atum\PurchaseOrders\Items\POItemFee;
+use Atum\PurchaseOrders\Items\POItemProduct;
+use Atum\PurchaseOrders\Items\POItemShipping;
 use Atum\PurchaseOrders\Models\PurchaseOrder;
 use Atum\PurchaseOrders\PurchaseOrders;
 
@@ -44,6 +47,40 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 	protected $post_type = PurchaseOrders::POST_TYPE;
 
 	/**
+	 * Allowed data keys for the PO
+	 *
+	 * @var array
+	 */
+	protected $rest_data_keys = array(
+		'id',
+		'status',
+		'currency',
+		'date_created',
+		'date_created_gmt',
+		'date_modified',
+		'date_modified_gmt',
+		'discount_total',
+		'discount_tax',
+		'shipping_total',
+		'shipping_tax',
+		'cart_tax',
+		'total',
+		'total_tax',
+		'prices_include_tax',
+		'date_completed',
+		'date_completed_gmt',
+		'supplier',
+		'multiple_suppliers',
+		'date_expected',
+		'date_expected_gmt',
+		'line_items',
+		'tax_lines',
+		'shipping_lines',
+		'fee_lines',
+		'meta_data',
+	);
+
+	/**
 	 * Get object. Return false if object is not of required type.
 	 *
 	 * @since  1.6.2
@@ -58,7 +95,7 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 
 		$po = new PurchaseOrder( $id );
 
-		// In case id is a refund's id (or it's not an order at all), don't expose it via /orders/ path.
+		// In case id is not a PO, don't expose it via /orders/ path.
 		if ( ! $po ) {
 			return FALSE;
 		}
@@ -69,80 +106,95 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 
 
 	/**
-	 * Prepare a single order for create or update.
+	 * Prepare a single purchase order for create or update.
 	 *
 	 * @since 1.6.2
 	 *
 	 * @param  \WP_REST_Request $request  Request object.
 	 * @param  bool             $creating If is creating a new object.
 	 *
-	 * @return \WP_Error|\WC_Data
+	 * @return \WP_Error|PurchaseOrder
 	 *
 	 * @throws \WC_REST_Exception When fails to set any item.
 	 */
 	protected function prepare_object_for_database( $request, $creating = FALSE ) {
 
 		$id        = isset( $request['id'] ) ? absint( $request['id'] ) : 0;
-		$order     = new WC_Order( $id );
+		$po        = $this->get_object( $id );
 		$schema    = $this->get_item_schema();
 		$data_keys = array_keys( array_filter( $schema['properties'], array( $this, 'filter_writable_props' ) ) );
 
 		// Handle all writable props.
 		foreach ( $data_keys as $key ) {
+
 			$value = $request[ $key ];
 
 			if ( ! is_null( $value ) ) {
+
 				switch ( $key ) {
-					case 'coupon_lines':
 					case 'status':
 						// Change should be done later so transitions have new data.
 						break;
-					case 'billing':
-					case 'shipping':
-						$this->update_address( $order, $value, $key );
-						break;
+
 					case 'line_items':
 					case 'shipping_lines':
 					case 'fee_lines':
 						if ( is_array( $value ) ) {
+
 							foreach ( $value as $item ) {
+
 								if ( is_array( $item ) ) {
+
 									if ( $this->item_is_null( $item ) || ( isset( $item['quantity'] ) && 0 === $item['quantity'] ) ) {
-										$order->remove_item( $item['id'] );
-									} else {
-										$this->set_item( $order, $key, $item );
+										$po->remove_item( $item['id'] );
 									}
+									else {
+										$this->set_item( $po, $key, $item );
+									}
+
 								}
+
 							}
+
 						}
+
 						break;
+
 					case 'meta_data':
-						if ( is_array( $value ) ) {
+						if ( is_array( $value ) && $id ) {
+
 							foreach ( $value as $meta ) {
-								$order->update_meta_data( $meta['key'], $meta['value'], isset( $meta['id'] ) ? $meta['id'] : '' );
+								$po->set_meta( $meta['key'], $meta['value'] );
 							}
+
 						}
+
 						break;
+
+					// date_expected || multiple_suppliers || supplier.
 					default:
-						if ( is_callable( array( $order, "set_{$key}" ) ) ) {
-							$order->{"set_{$key}"}( $value );
+						if ( is_callable( array( $po, "set_{$key}" ) ) ) {
+							$po->{"set_{$key}"}( $value );
 						}
+
 						break;
 				}
+
 			}
+
 		}
 
 		/**
 		 * Filters an object before it is inserted via the REST API.
 		 *
-		 * The dynamic portion of the hook name, `$this->post_type`,
-		 * refers to the object type slug.
+		 * The dynamic portion of the hook name, `$this->post_type`, refers to the object type slug.
 		 *
-		 * @param \WC_Data         $order    Object object.
+		 * @param PurchaseOrder    $po       Object object.
 		 * @param \WP_REST_Request $request  Request object.
 		 * @param bool             $creating If is creating a new object.
 		 */
-		return apply_filters( "atum/api/rest_pre_insert_{$this->post_type}_object", $order, $request, $creating );
+		return apply_filters( "atum/api/rest_pre_insert_{$this->post_type}_object", $po, $request, $creating );
+
 	}
 
 	/**
@@ -153,34 +205,18 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 	 * @param  \WP_REST_Request $request  Full details about the request.
 	 * @param  bool             $creating If is creating a new object.
 	 *
-	 * @return \WC_Data|\WP_Error
+	 * @return PurchaseOrder|\WP_Error
 	 *
-	 * @throws \WC_REST_Exception But all errors are validated before returning any data.
+	 * @throws \WC_REST_Exception|\WC_Data_Exception But all errors are validated before returning any data.
 	 */
 	protected function save_object( $request, $creating = FALSE ) {
 
 		try {
+
 			$object = $this->prepare_object_for_database( $request, $creating );
 
 			if ( is_wp_error( $object ) ) {
 				return $object;
-			}
-
-			// Make sure gateways are loaded so hooks from gateways fire on save/create.
-			wc()->payment_gateways();
-
-			if ( ! is_null( $request['customer_id'] ) && 0 !== $request['customer_id'] ) {
-
-				// Make sure customer exists.
-				if ( false === get_user_by( 'id', $request['customer_id'] ) ) {
-					throw new \WC_REST_Exception( 'atum_rest_invalid_customer_id', __( 'Customer ID is invalid.', ATUM_TEXT_DOMAIN ), 400 );
-				}
-
-				// Make sure customer is part of blog.
-				if ( is_multisite() && ! is_user_member_of_blog( $request['customer_id'] ) ) {
-					add_user_to_blog( get_current_blog_id(), $request['customer_id'], 'customer' );
-				}
-
 			}
 
 			if ( $creating ) {
@@ -188,15 +224,10 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 				$object->set_prices_include_tax( 'yes' === get_option( 'woocommerce_prices_include_tax' ) );
 				$object->calculate_totals();
 			}
-			else {
-				// If items have changed, recalculate order totals.
-				if ( isset( $request['billing'] ) || isset( $request['shipping'] ) || isset( $request['line_items'] ) || isset( $request['shipping_lines'] ) || isset( $request['fee_lines'] ) || isset( $request['coupon_lines'] ) ) {
-					$object->calculate_totals( TRUE );
-				}
+			// If items have changed, recalculate order totals.
+			elseif ( isset( $request['line_items'] ) || isset( $request['shipping_lines'] ) || isset( $request['fee_lines'] ) ) {
+				$object->calculate_totals( TRUE );
 			}
-
-			// Set coupons.
-			$this->calculate_coupons( $request, $object );
 
 			// Set status.
 			if ( ! empty( $request['status'] ) ) {
@@ -204,13 +235,6 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 			}
 
 			$object->save();
-
-			// Actions for after the order is saved.
-			if ( true === $request['set_paid'] ) {
-				if ( $creating || $object->needs_payment() ) {
-					$object->payment_complete( $request['transaction_id'] );
-				}
-			}
 
 			return $this->get_object( $object->get_id() );
 
@@ -266,7 +290,7 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 			// phpcs:disable WordPress.DB.PreparedSQL
 			$order_ids = $wpdb->get_col(
 				$wpdb->prepare( "
-					SELECT order_id
+					SELECT DISTINCT order_id
 					FROM $atum_order_items_table
 					WHERE order_item_id IN ( 
 						SELECT order_item_id FROM $atum_order_item_meta_table WHERE meta_key = '_product_id' AND meta_value = %d 
@@ -275,8 +299,8 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 						SELECT order_id FROM $wpdb->posts WHERE post_type = %s
 					)
 					AND order_item_type = 'line_item'",
-					$this->post_type,
-					$request['product']
+					$request['product'],
+					$this->post_type
 				)
 			);
 			// phpcs:enable
@@ -298,6 +322,19 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 				unset( $args['s'] );
 				$args['post__in'] = array_merge( $order_ids, [ 0 ] );
 			}
+
+		}
+
+		// Expected at location date.
+		if ( ! empty( $request['date_expected'] ) ) {
+
+			$args['meta_query'] = array(
+				array(
+					'key'   => '_expected_at_location_date',
+					'value' => $request['date_expected'],
+					'type'  => 'DATETIME',
+				),
+			);
 
 		}
 
@@ -328,7 +365,9 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 	 */
 	protected function get_formatted_item_data( $object ) {
 
-		$data              = $object->get_data();
+		$data       = $object->get_data();
+		$data['id'] = $object->get_id();
+
 		$format_decimal    = array( 'discount_total', 'discount_tax', 'shipping_total', 'shipping_tax', 'cart_tax', 'total', 'total_tax' );
 		$format_date       = array( 'date_created', 'date_modified', 'date_completed', 'date_expected' );
 		$format_line_items = array( 'line_items', 'tax_lines', 'shipping_lines', 'fee_lines' );
@@ -350,44 +389,17 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 			$data[ $key ] = array_values( array_map( array( $this, 'get_order_item_data' ), $data[ $key ] ) );
 		}
 
-		// Refunds.
-		// TODO??
-		/*$data['refunds'] = array();
-		foreach ( $object->get_refunds() as $refund ) {
-			$data['refunds'][] = array(
-				'id'     => $refund->get_id(),
-				'reason' => $refund->get_reason() ? $refund->get_reason() : '',
-				'total'  => '-' . wc_format_decimal( $refund->get_amount(), $this->request['dp'] ),
-			);
-		}*/
+		$formatted_data = array();
 
-		return array(
-			'id'                 => $object->get_id(),
-			'status'             => $data['status'],
-			'currency'           => $data['currency'],
-			'date_created'       => $data['date_created'],
-			'date_created_gmt'   => $data['date_created_gmt'],
-			'date_modified'      => $data['date_modified'],
-			'date_modified_gmt'  => $data['date_modified_gmt'],
-			'discount_total'     => $data['discount_total'],
-			'discount_tax'       => $data['discount_tax'],
-			'shipping_total'     => $data['shipping_total'],
-			'shipping_tax'       => $data['shipping_tax'],
-			'cart_tax'           => $data['cart_tax'],
-			'total'              => $data['total'],
-			'total_tax'          => $data['total_tax'],
-			'prices_include_tax' => $data['prices_include_tax'],
-			'date_completed'     => $data['date_completed'],
-			'date_completed_gmt' => $data['date_completed_gmt'],
-			'supplier'           => $data['supplier'],
-			'multiple_suppliers' => $data['multiple_suppliers'],
-			'date_expected'      => $data['date_expected'],
-			'date_expected_gmt'  => $data['date_expected_gmt'],
-			'line_items'         => $data['line_items'],
-			'tax_lines'          => $data['tax_lines'],
-			'shipping_lines'     => $data['shipping_lines'],
-			'fee_lines'          => $data['fee_lines'],
-		);
+		foreach ( $data as $data_key => $data_value ) {
+
+			if ( in_array( $data_key, $this->rest_data_keys ) ) {
+				$formatted_data[ $data_key ] = $data_value;
+			}
+
+		}
+
+		return $formatted_data;
 
 	}
 
@@ -402,7 +414,35 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 
 		$schema = parent::get_item_schema();
 
-		$schema['properties']['coupon_lines']['items']['properties']['discount']['readonly'] = TRUE;
+		// Remove unneeded properties from the schema.
+		foreach ( $schema['properties'] as $key => $value ) {
+
+			if ( ! in_array( $key, $this->rest_data_keys ) ) {
+				unset( $schema['properties'][ $key ] );
+			}
+
+		}
+
+		$schema['properties']['status']['default'] = ATUM_PREFIX . 'pending';
+		$schema['properties']['status']['enum']    = array_keys( Helpers::get_atum_order_post_type_statuses( $this->post_type ) );
+
+		$schema['properties']['date_expected'] = array(
+			'description' => __( 'The date when the purchase order is expected at location.', ATUM_TEXT_DOMAIN ),
+			'type'        => 'date-time',
+			'context'     => array( 'view', 'edit' ),
+		);
+
+		$schema['properties']['supplier'] = array(
+			'description' => __( 'The supplier ID linked to the purchase order.', ATUM_TEXT_DOMAIN ),
+			'type'        => 'integer',
+			'context'     => array( 'view', 'edit' ),
+		);
+
+		$schema['properties']['multiple_suppliers'] = array(
+			'description' => __( 'Whether the multiple_suppliers switch is enabled or not.', ATUM_TEXT_DOMAIN ),
+			'type'        => 'boolean',
+			'context'     => array( 'view', 'edit' ),
+		);
 
 		return $schema;
 
@@ -419,14 +459,37 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 
 		$params = parent::get_collection_params();
 
+		// We don't need the customer here.
+		unset( $params['customer'] );
+
 		$params['status'] = array(
 			'default'           => 'any',
-			'description'       => __( 'Limit result set to orders which have specific statuses.', ATUM_TEXT_DOMAIN ),
+			'description'       => __( 'Limit result set to purchase orders which have specific statuses.', ATUM_TEXT_DOMAIN ),
 			'type'              => 'array',
 			'items'             => array(
 				'type' => 'string',
-				'enum' => array_merge( array( 'any', 'trash' ), $this->get_order_statuses() ),
+				'enum' => array_merge( [ 'any', 'trash' ], array_keys( Helpers::get_atum_order_post_type_statuses( $this->post_type ) ) ),
 			),
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		$params['date_expected'] = array(
+			'description'       => __( 'Limit result set to purchase orders expected at location on a given ISO8601 compliant date.', ATUM_TEXT_DOMAIN ),
+			'type'              => 'string',
+			'format'            => 'date-time',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		$params['supplier'] = array(
+			'description'       => __( 'Limit result set to purchase orders linked to the specified supplier ID.', ATUM_TEXT_DOMAIN ),
+			'type'              => 'integer',
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		$params['multiple_suppliers'] = array(
+			'description'       => __( 'Limit result set to purchase orders depending on their multiple_suppliers switch status.', ATUM_TEXT_DOMAIN ),
+			'type'              => 'boolean',
 			'validate_callback' => 'rest_validate_request_arg',
 		);
 
@@ -456,6 +519,146 @@ class PurchaseOrdersController extends \WC_REST_Orders_Controller {
 		);
 
 		return $links;
+
+	}
+
+	/**
+	 * Wrapper method to create/update order items.
+	 * When updating, the item ID provided is checked to ensure it is associated with the purchase order.
+	 *
+	 * @since 1.6.2
+	 *
+	 * @param PurchaseOrder $po        Purchase Order object.
+	 * @param string        $item_type The item type.
+	 * @param array         $posted    Item provided in the request body.
+	 *
+	 * @throws \WC_REST_Exception If item ID is not associated with the purchase order.
+	 */
+	protected function set_item( $po, $item_type, $posted ) {
+
+		$action = ! empty( $posted['id'] ) ? 'update' : 'create';
+		$method = "prepare_$item_type";
+		$item   = NULL;
+
+		// Verify provided line item ID is associated with PO.
+		if ( 'update' === $action ) {
+
+			$item = $po->get_atum_order_item( absint( $posted['id'] ), FALSE );
+
+			if ( ! $item ) {
+				throw new \WC_REST_Exception( 'atum_rest_invalid_item_id', __( 'Order item ID provided is not associated with purchase order.', ATUM_TEXT_DOMAIN ), 400 );
+			}
+
+		}
+
+		// Prepare item data.
+		$item = $this->$method( $posted, $action, $item );
+
+		do_action( 'atum/api/rest_set_order_item', $item, $posted );
+
+		// If updating the PO, add the item to it.
+		if ( 'update' === $action ) {
+			$item->save();
+		}
+
+		// Add/Update the item on the PO object.
+		$po->add_item( $item );
+
+	}
+
+	/**
+	 * Create or update a line item
+	 *
+	 * @since 1.6.2
+	 *
+	 * @param array  $posted Line item data.
+	 * @param string $action 'create' to add line item or 'update' to update it.
+	 * @param object $item Passed when updating an item. Null during creation.
+	 *
+	 * @return POItemProduct
+	 *
+	 * @throws \WC_REST_Exception Invalid data, server error.
+	 */
+	protected function prepare_line_items( $posted, $action = 'create', $item = NULL ) {
+
+		$item    = is_null( $item ) ? new POItemProduct( ! empty( $posted['id'] ) ? $posted['id'] : '' ) : $item;
+		$product = Helpers::get_atum_product( $this->get_product_id( $posted ) );
+
+		if ( $product !== $item->get_product() ) {
+
+			$item->set_product( $product );
+
+			if ( 'create' === $action ) {
+				$quantity = isset( $posted['quantity'] ) ? $posted['quantity'] : 1;
+				$total    = wc_get_price_excluding_tax( $product, array( 'qty' => $quantity ) );
+				$item->set_total( $total );
+				$item->set_subtotal( $total );
+			}
+
+		}
+
+		$this->maybe_set_item_props( $item, array( 'name', 'quantity', 'total', 'subtotal', 'tax_class' ), $posted );
+		$this->maybe_set_item_meta_data( $item, $posted );
+
+		return $item;
+
+	}
+
+	/**
+	 * Create or update an order shipping method
+	 *
+	 * @since 1.6.2
+	 *
+	 * @param array  $posted $shipping Item data.
+	 * @param string $action 'create' to add shipping or 'update' to update it.
+	 * @param object $item Passed when updating an item. Null during creation.
+	 *
+	 * @return POItemShipping
+	 *
+	 * @throws \WC_REST_Exception Invalid data, server error.
+	 */
+	protected function prepare_shipping_lines( $posted, $action = 'create', $item = NULL ) {
+
+		$item = is_null( $item ) ? new POItemShipping( ! empty( $posted['id'] ) ? $posted['id'] : '' ) : $item;
+
+		if ( 'create' === $action ) {
+			if ( empty( $posted['method_id'] ) ) {
+				throw new \WC_REST_Exception( 'atum_rest_invalid_shipping_item', __( 'Shipping method ID is required.', ATUM_TEXT_DOMAIN ), 400 );
+			}
+		}
+
+		$this->maybe_set_item_props( $item, array( 'method_id', 'method_title', 'total' ), $posted );
+		$this->maybe_set_item_meta_data( $item, $posted );
+
+		return $item;
+
+	}
+
+	/**
+	 * Create or update an order fee
+	 *
+	 * @since 1.6.2
+	 *
+	 * @param array  $posted Item data.
+	 * @param string $action 'create' to add fee or 'update' to update it.
+	 * @param object $item Passed when updating an item. Null during creation.
+	 *
+	 * @return POItemFee
+	 *
+	 * @throws \WC_REST_Exception Invalid data, server error.
+	 */
+	protected function prepare_fee_lines( $posted, $action = 'create', $item = NULL ) {
+
+		$item = is_null( $item ) ? new POItemFee( ! empty( $posted['id'] ) ? $posted['id'] : '' ) : $item;
+
+		if ( 'create' === $action && empty( $posted['name'] ) ) {
+			throw new \WC_REST_Exception( 'atum_rest_invalid_fee_item', __( 'Fee name is required.', ATUM_TEXT_DOMAIN ), 400 );
+		}
+
+		$this->maybe_set_item_props( $item, array( 'name', 'tax_class', 'tax_status', 'total' ), $posted );
+		$this->maybe_set_item_meta_data( $item, $posted );
+
+		return $item;
 
 	}
 
