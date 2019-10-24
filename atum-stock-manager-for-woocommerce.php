1d1fd9d5749400107755f2ee75795cdd5dd7b2cc
Salva Machí <salvamb@berebel.io>
2019-10-24 13:57:56 +0200

    Update WP version compatibility

--- atum-stock-manager-for-woocommerce.php
+++ atum-stock-manager-for-woocommerce.php
@@ -15,7 +15,7 @@
  * Author URI:           https://www.stockmanagementlabs.com/
  * Contributors:         Be Rebel Studio - https://berebel.io
  * Requires at least:    4.4
- * Tested up to:         5.2.4
+ * Tested up to:         5.3.0
  * Requires PHP:         5.6
  * WC requires at least: 2.7.0
  * WC tested up to:      3.7.1
