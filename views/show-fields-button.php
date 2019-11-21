165262e618c866908833d447a02de61c1b58af3e
José Andrés Piera <japiera@sispixels.com>
2019-11-21 13:34:39 +0100

    Add Post type list CSS for mobile

---
+++ views/show-fields-button.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * View for the Stock Central page
+ *
+ * @since 1.6.4
+ */
+
+?>
+<div class="alignleft show-filters-wrapper">
+	<span><?php esc_html_e( 'Filters', ATUM_TEXT_DOMAIN ) ?></span>
+	<button type="button" class="show-filters btn btn-link no-drag" data-action="show">
+		<?php esc_html_e( 'Show', ATUM_TEXT_DOMAIN ) ?>
+	</button>
+</div>
