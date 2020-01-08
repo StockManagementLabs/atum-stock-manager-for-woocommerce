ba92fea075e2eed7cc452970a3b97c04884546b1
Salva Machí <salvamb@berebel.io>
2020-01-08 13:19:40 +0100

    Re-added missing utility

--- assets/js/src/utils/_utils.ts
+++ assets/js/src/utils/_utils.ts
@@ -209,6 +209,27 @@ export let Utils = {
 		
 		// If we made it this far, objects are considered equivalent.
 		return true;
+	},
+	
+	/**
+	 * Toggle tree nodes
+	 *
+	 * @param {any[]}  nodes
+	 * @param {string} openOrClose
+	 */
+	toggleNodes(nodes: any[], openOrClose: string){
+		
+		for (let i: number = 0; i < nodes.length; i++) {
+			
+			nodes[i].isExpanded = openOrClose == 'open'; // Either expand node or don't
+			
+			// If has children open/close those as well.
+			if (nodes[i].children && nodes[i].children.length > 0) {
+				this.toggleNodes(nodes[i].children, openOrClose);
+			}
+			
+		}
+		
 	}
 	
 }
