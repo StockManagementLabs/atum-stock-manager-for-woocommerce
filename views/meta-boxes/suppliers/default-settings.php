16bcbd73e9f6a19f95dccea95a1754fe327e4db8
Salva Machí <salvamb@berebel.io>
2020-03-06 12:34:08 +0100

    Added extra fields to Supplier profile

--- views/meta-boxes/suppliers/default-settings.php
+++ views/meta-boxes/suppliers/default-settings.php
@@ -25,13 +25,28 @@ defined( 'ABSPATH' ) || die;
 			'id'               => 'assigned_to',
 			'class'            => 'wc-enhanced-select atum-enhanced-select',
 		);
-		wp_dropdown_users( $args );
+		wp_dropdown_users( $args ); // TODO: THIS CAN CAUSE PERFORMANCE ISSUES IF THERE ARE THOUSANDS USERS.
 		?>
 	</div>
 
 	<div class="form-field form-field-wide">
 		<label for="location"><?php esc_html_e( 'Location', ATUM_TEXT_DOMAIN ) ?></label>
 		<input type="text" id="location" name="default_settings[location]" value="<?php echo esc_attr( $supplier->location ) ?>">
+	</div>
+
+	<div class="form-field form-field-wide">
+		<label for="discount"><?php esc_html_e( 'Discount', ATUM_TEXT_DOMAIN ) ?></label>
+		<input type="number" step="1" min="0" id="discount" name="default_settings[discount]" value="<?php echo esc_attr( $supplier->discount ) ?>">
+	</div>
+
+	<div class="form-field form-field-wide">
+		<label for="tax_rate"><?php esc_html_e( 'Tax Rate', ATUM_TEXT_DOMAIN ) ?></label>
+		<input type="number" step="1" min="0" id="tax_rate" name="default_settings[tax_rate]" value="<?php echo esc_attr( $supplier->tax_rate ) ?>">
+	</div>
+
+	<div class="form-field form-field-wide">
+		<label for="lead_time"><?php esc_html_e( 'Lead Time (in days)', ATUM_TEXT_DOMAIN ) ?></label>
+		<input type="number" step="1" min="0" id="lead_time" name="default_settings[lead_time]" value="<?php echo esc_attr( $supplier->lead_time ) ?>">
 	</div>
 
 </div>
