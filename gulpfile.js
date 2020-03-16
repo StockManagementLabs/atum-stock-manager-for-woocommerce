3afd1dbfa01186d54b96d4df40a3902037805e5d
Salva Machí <salvamb@berebel.io>
2020-03-16 11:30:18 +0100

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var gulp          = require('gulp'),
 	path          = require('path');
 
 // Plugin version
-var version = '1.6.9',
+var version = '1.6.10',
     curDate = new Date();
 
 // Global config
