7046d6dafe4662890398bd05f080037c0d200960
Salva Machi <salvamb@berebel.io>
2020-03-27 10:55:29 +0100

    Refactory

--- assets/js/src/components/orders/_order-items.ts
+++ assets/js/src/components/orders/_order-items.ts
@@ -338,17 +338,17 @@ export default class AtumOrderItems {
 	setPurchasePrice( evt: JQueryEventObject, purchasePrice?: number, purchasePriceTxt?: string ) {
 		
 		evt.preventDefault();
-		
-		let $item: JQuery            = $(evt.currentTarget).closest('.item'),
-		    qty: number              = parseFloat($item.find('input.quantity').val() || 1),
-		    taxes: number            = 0,
-		    data: any                = {
-			    atum_order_id     : this.settings.get('post_id'),
-			    atum_order_item_id: $item.data('atum_order_item_id'),
+
+		let $item: JQuery = $( evt.currentTarget ).closest( '.item' ),
+		    qty: number   = parseFloat( $item.find( 'input.quantity' ).val() || 1 ),
+		    taxes: number = 0,
+		    data: any     = {
+			    atum_order_id     : this.settings.get( 'post_id' ),
+			    atum_order_item_id: $item.data( 'atum_order_item_id' ),
 			    action            : 'atum_order_change_purchase_price',
-			    security          : this.settings.get('atum_order_item_nonce'),
+			    security          : this.settings.get( 'atum_order_item_nonce' ),
 		    },
-		    rates: any               = $item.find('.item_cost').data('productTaxRates');
+		    rates: any    = $item.find( '.item_cost' ).data( 'productTaxRates' );
 
 		if ( ! purchasePrice ) {
 			purchasePrice = qty !== 0 ? <number>Utils.unformat($item.find('input.line_total').val() || 0, this.settings.get('mon_decimal_point')) / qty : 0;
