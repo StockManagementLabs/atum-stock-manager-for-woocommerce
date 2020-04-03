120a83537287028f14eb331e9be37bca36e1f91c
Salva Machi <salvamb@berebel.io>
2020-04-03 12:40:20 +0200

    Added custom triggers after running a bulk action

--- assets/js/src/components/list-table/_bulk-actions.ts
+++ assets/js/src/components/list-table/_bulk-actions.ts
@@ -2,9 +2,9 @@
    BULK ACTIONS FOR LIST TABLES
    ======================================= */
 
-import Settings from '../../config/_settings';
 import Globals from './_globals';
 import ListTable from './_list-table';
+import Settings from '../../config/_settings';
 import { Utils } from '../../utils/_utils';
 
 export default class BulkActions {
@@ -20,49 +20,49 @@ export default class BulkActions {
 			//
 			// Apply Bulk Actions.
 			// -------------------
-			.on('click', '.apply-bulk-action', () => {
-				
-				if (!this.globals.$atumList.find('.check-column input:checked').length) {
-					
-					const swal: any = window['swal'];
-					
-					swal({
-						title             : this.settings.get('noItemsSelected'),
-						text              : this.settings.get('selectItems'),
+			.on( 'click', '.apply-bulk-action', () => {
+
+				if ( ! this.globals.$atumList.find( '.check-column input:checked' ).length ) {
+
+					const swal: any = window[ 'swal' ];
+
+					swal( {
+						title             : this.settings.get( 'noItemsSelected' ),
+						text              : this.settings.get( 'selectItems' ),
 						type              : 'info',
-						confirmButtonText : this.settings.get('ok'),
+						confirmButtonText : this.settings.get( 'ok' ),
 						confirmButtonColor: 'var(--primary)',
-					});
-					
+					} );
+
 				}
 				else {
 					this.applyBulk();
 				}
-				
-			})
+
+			} )
 			
 			//
 			// Bulk actions dropdown.
 			// ----------------------
-			.on('change', '.bulkactions select', (evt: JQueryEventObject) => {
-				
+			.on( 'change', '.bulkactions select', ( evt: JQueryEventObject ) => {
+
 				this.updateBulkButton();
-				
-				if ($(evt.currentTarget).val() !== '-1') {
-					$('.apply-bulk-action').show();
+
+				if ( $( evt.currentTarget ).val() !== '-1' ) {
+					$( '.apply-bulk-action' ).show();
 				}
 				else {
-					$('.apply-bulk-action').hide();
+					$( '.apply-bulk-action' ).hide();
 				}
-				
-			})
+
+			} )
 			
 			//
 			// Change the Bulk Button text when selecting boxes.
 			// -------------------------------------------------
-			.on('change', '.check-column input:checkbox', () => {
+			.on( 'change', '.check-column input:checkbox', () => {
 				this.updateBulkButton();
-			});
+			} );
 		
 	}
 	
@@ -70,51 +70,56 @@ export default class BulkActions {
 	 * Apply a bulk action for the selected rows
 	 */
 	applyBulk() {
-		
-		let $bulkButton: JQuery     = $('.apply-bulk-action'),
-		    bulkAction: string      = this.globals.$atumList.find('.bulkactions select').filter((index: number, elem: Element) => {
-			    return $(elem).val() !== '-1'
-		    }).val(),
+
+		let $bulkButton: JQuery     = $( '.apply-bulk-action' ),
+		    bulkAction: string      = this.globals.$atumList.find( '.bulkactions select' ).filter( ( index: number, elem: Element ) => {
+			    return $( elem ).val() !== '-1';
+		    } ).val(),
 		    selectedItems: string[] = [];
-		
-		this.globals.$atumList.find('tbody .check-column input:checkbox').filter(':checked').each( (index: number, elem: Element) => {
-			selectedItems.push( $(elem).val() );
-		});
-		
-		$.ajax({
-			url       : window['ajaxurl'],
+
+		this.globals.$atumList.find( 'tbody .check-column input:checkbox' ).filter( ':checked' ).each( ( index: number, elem: Element ) => {
+			selectedItems.push( $( elem ).val() );
+		} );
+
+		$.ajax( {
+			url       : window[ 'ajaxurl' ],
 			method    : 'POST',
 			dataType  : 'json',
 			data      : {
-				token      : this.settings.get('nonce'),
+				token      : this.settings.get( 'nonce' ),
 				action     : 'atum_apply_bulk_action',
 				bulk_action: bulkAction,
 				ids        : selectedItems,
 			},
 			beforeSend: () => {
-				$bulkButton.prop('disabled', true);
+				$bulkButton.prop( 'disabled', true );
 				this.listTable.addOverlay();
 			},
-			success   : (response: any) => {
-				
-				if (typeof response === 'object') {
+			success   : ( response: any ) => {
+
+				if ( typeof response === 'object' ) {
 					const noticeType = response.success ? 'updated' : 'error';
-					Utils.addNotice(noticeType, response.data);
+					Utils.addNotice( noticeType, response.data );
 				}
-				
-				$bulkButton.prop('disabled', false);
-				
-				if (response.success) {
+
+				$bulkButton.prop( 'disabled', false );
+
+				if ( response.success ) {
 					$bulkButton.hide();
 					this.listTable.updateTable();
+					$bulkButton.trigger( 'atum-list-table-bulk-actions-success', [ bulkAction, selectedItems ] );
 				}
-				
+				else {
+					$bulkButton.trigger( 'atum-list-table-bulk-actions-error', [ bulkAction, selectedItems ] );
+				}
+
 			},
 			error     : () => {
-				$bulkButton.prop('disabled', false);
+				$bulkButton.prop( 'disabled', false );
 				this.listTable.removeOverlay();
+				$bulkButton.trigger( 'atum-list-table-bulk-actions-error', [ bulkAction, selectedItems ] );
 			},
-		})
+		} );
 		
 	}
 	
@@ -122,11 +127,11 @@ export default class BulkActions {
 	 * Update the Bulk Button text depending on the number of checkboxes selected
 	 */
 	updateBulkButton() {
-		
-		let numChecked: number = this.globals.$atumList.find('.check-column input:checkbox:checked').length,
-		    buttonText: string = numChecked > 1 ? this.settings.get('applyBulkAction') : this.settings.get('applyAction');
-		
-		$('.apply-bulk-action').text(buttonText);
+
+		let numChecked: number = this.globals.$atumList.find( '.check-column input:checkbox:checked' ).length,
+		    buttonText: string = numChecked > 1 ? this.settings.get( 'applyBulkAction' ) : this.settings.get( 'applyAction' );
+
+		$( '.apply-bulk-action' ).text( buttonText );
 		
 	}
 	
