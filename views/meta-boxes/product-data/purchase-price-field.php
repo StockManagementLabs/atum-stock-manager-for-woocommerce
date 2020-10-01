350dd0e9714b96f68b46b9a96c316a996f6a0884
Miguel Angel <miguelangel@berebel.io>
2020-10-01 11:35:33 +0200

    Adding filter for possible hiding invalid class in the Purchase Price field

--- views/meta-boxes/product-data/purchase-price-field.php
+++ views/meta-boxes/product-data/purchase-price-field.php
@@ -12,21 +12,24 @@
  * @var string   $field_id
  * @var \WP_Post $variation
  * @var int      $loop
+ * @var int      $product_id
  */
 
 defined( 'ABSPATH' ) || die;
 
 use Atum\Inc\Helpers;
+
+$invalid_purchase_price = apply_filters( 'atum/meta_boxes/purchase_price/invalid', ( $field_value > $price ), $product_id );
 ?>
 <p class="form-field <?php echo esc_attr( $wrapper_class ) ?>">
 	<label for="<?php echo esc_attr( $field_id ) ?>"><?php echo esc_html( $field_title ) ?></label>
 
-	<span class="atum-field input-group<?php if ($field_value > $price) echo ' invalid' ?>">
+	<span class="atum-field input-group<?php if ($invalid_purchase_price) echo ' invalid' ?>">
 		<?php Helpers::atum_field_input_addon() ?>
 
-		<input type="text" class="short wc_input_price<?php if ( $field_value > $price ) echo ' tips' ?>" name="<?php echo esc_attr( $field_name ) ?>"
+		<input type="text" class="short wc_input_price<?php if ( $invalid_purchase_price ) echo ' tips' ?>" name="<?php echo esc_attr( $field_name ) ?>"
 			id="<?php echo esc_attr( $field_id ) ?>" value="<?php echo esc_attr( wc_format_localized_price( $field_value ) ) ?>" placeholder=""
-			<?php if ( $field_value > $price ) echo ' data-tip="' . esc_attr__( "The Purchase Price set is greater than the product's active price", ATUM_TEXT_DOMAIN ) . '"' ?>
+			<?php if ( $invalid_purchase_price ) echo ' data-tip="' . esc_attr__( "The Purchase Price set is greater than the product's active price", ATUM_TEXT_DOMAIN ) . '"' ?>
 			<?php echo apply_filters( 'atum/views/meta_boxes/purchase_price_field_extra_atts', '', $variation, $loop ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 			>
 	</span>
