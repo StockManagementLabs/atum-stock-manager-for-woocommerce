10f67a6ab27cf3e93cb7a458807f4983cc441f71
José Andrés Piera <japiera@sispixels.com>
2021-01-21 12:42:59 +0100

    Prevent scientific notation in Purchase Price

--- views/meta-boxes/product-data/purchase-price-field.php
+++ views/meta-boxes/product-data/purchase-price-field.php
@@ -19,6 +19,10 @@ defined( 'ABSPATH' ) || die;
 
 use Atum\Inc\Helpers;
 
+$decimals           = \Atum\Inc\Globals::get_prices_decimals();
+$decimal_separator  = wc_get_price_decimal_separator();
+$thousand_separator = wc_get_price_thousand_separator();
+
 $invalid_purchase_price = apply_filters( 'atum/meta_boxes/purchase_price/invalid', ( $field_value > $price ), $product_id );
 ?>
 <p class="form-field <?php echo esc_attr( $wrapper_class ) ?>">
@@ -28,9 +32,9 @@ $invalid_purchase_price = apply_filters( 'atum/meta_boxes/purchase_price/invalid
 		<?php Helpers::atum_field_input_addon() ?>
 
 		<input type="text" class="short wc_input_price<?php if ( $invalid_purchase_price ) echo ' tips' ?>" name="<?php echo esc_attr( $field_name ) ?>"
-			id="<?php echo esc_attr( $field_id ) ?>" value="<?php echo esc_attr( wc_format_localized_price( $field_value ) ) ?>" placeholder=""
+			id="<?php echo esc_attr( $field_id ) ?>" value="<?php echo esc_attr( number_format( $field_value, $decimals, $decimal_separator, $thousand_separator ) ) ?>" placeholder=""
 			<?php if ( $invalid_purchase_price ) echo ' data-tip="' . esc_attr__( "The Purchase Price set is greater than the product's active price", ATUM_TEXT_DOMAIN ) . '"' ?>
-			<?php echo apply_filters( 'atum/views/meta_boxes/purchase_price_field_extra_atts', '', $variation, $loop ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			<?php echo apply_filters( 'atum/views/meta_boxes/purchase_price_field_extra_atts', '', $variation, $loop, $product_id ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 			>
 	</span>
 </p>
