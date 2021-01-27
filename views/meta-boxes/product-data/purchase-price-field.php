7728a5067fb08fd8b832d0d22103f20db5d1273c
José Andrés Piera <japiera@sispixels.com>
2021-01-27 12:20:53 +0100

    Prevent PP no numeric notices

--- views/meta-boxes/product-data/purchase-price-field.php
+++ views/meta-boxes/product-data/purchase-price-field.php
@@ -24,6 +24,8 @@ $decimal_separator  = wc_get_price_decimal_separator();
 $thousand_separator = wc_get_price_thousand_separator();
 
 $invalid_purchase_price = apply_filters( 'atum/meta_boxes/purchase_price/invalid', ( $field_value > $price ), $product_id );
+$purchase_price         = is_numeric( $field_value ) ? number_format( $field_value, $decimals, $decimal_separator, $thousand_separator ) : '';
+
 ?>
 <p class="form-field <?php echo esc_attr( $wrapper_class ) ?>">
 	<label for="<?php echo esc_attr( $field_id ) ?>"><?php echo esc_html( $field_title ) ?></label>
@@ -32,7 +34,7 @@ $invalid_purchase_price = apply_filters( 'atum/meta_boxes/purchase_price/invalid
 		<?php Helpers::atum_field_input_addon() ?>
 
 		<input type="text" class="short wc_input_price<?php if ( $invalid_purchase_price ) echo ' tips' ?>" name="<?php echo esc_attr( $field_name ) ?>"
-			id="<?php echo esc_attr( $field_id ) ?>" value="<?php echo esc_attr( number_format( $field_value, $decimals, $decimal_separator, $thousand_separator ) ) ?>" placeholder=""
+			id="<?php echo esc_attr( $field_id ) ?>" value="<?php echo esc_attr( $purchase_price ) ?>" placeholder=""
 			<?php if ( $invalid_purchase_price ) echo ' data-tip="' . esc_attr__( "The Purchase Price set is greater than the product's active price", ATUM_TEXT_DOMAIN ) . '"' ?>
 			<?php echo apply_filters( 'atum/views/meta_boxes/purchase_price_field_extra_atts', '', $variation, $loop, $product_id ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 			>
