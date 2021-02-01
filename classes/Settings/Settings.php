ead30321f25dba5057301c2c1c02914f0d77a4c9
Salva Machí <salvamb@berebel.io>
2021-02-01 13:09:56 +0100

    Text changes

--- classes/Settings/Settings.php
+++ classes/Settings/Settings.php
@@ -306,7 +306,7 @@ class Settings {
 				'group'   => 'general',
 				'section' => 'general',
 				'name'    => __( 'Show Totals Row', ATUM_TEXT_DOMAIN ),
-				'desc'    => __( 'When enabled, ATUM will display new row at the bottom of Stock Central. You will be able to preview page column totals of essential stock counters.', ATUM_TEXT_DOMAIN ),
+				'desc'    => __( 'When enabled, ATUM will display new row at the bottom of all the List Tables. You will be able to preview page column totals of essential stock counters.', ATUM_TEXT_DOMAIN ),
 				'type'    => 'switcher',
 				'default' => 'yes',
 			),
@@ -400,7 +400,7 @@ class Settings {
 				'group'   => 'general',
 				'section' => 'general',
 				'name'    => __( 'Show sales in the last selected days', ATUM_TEXT_DOMAIN ),
-				'desc'    => __( 'Enter the number of days to calculate the number of sales in that period in one Stock Central column.', ATUM_TEXT_DOMAIN ),
+				'desc'    => __( 'Enter the number of days to calculate the number of sales in that period in ATUM List Tables.', ATUM_TEXT_DOMAIN ),
 				'type'    => 'number',
 				'default' => self::DEFAULT_SALE_DAYS,
 				'options' => array(
