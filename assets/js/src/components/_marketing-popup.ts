086204bbe34828439f3dbcfad11b046c81bf364f
Salva Machí <salvamb@berebel.io>
2021-04-08 10:51:58 +0200

    Added new marketing popup

--- assets/js/src/components/_marketing-popup.ts
+++ assets/js/src/components/_marketing-popup.ts
@@ -59,101 +59,105 @@ export default class MarketingPopup {
 	getPopupInfo() {
 		
 		$.ajax({
-			url       : window['ajaxurl'],
-			dataType  : 'json',
-			method    : 'post',
-			data      : {
-				action    : 'atum_get_marketing_popup_info',
-				token     : this.settings.get('nonce'),
+			url     : window[ 'ajaxurl' ],
+			dataType: 'json',
+			method  : 'post',
+			data    : {
+				action: 'atum_get_marketing_popup_info',
+				token : this.settings.get( 'nonce' ),
 			},
-			success   : (response: any) => {
-				
-				if (response.success === true) {
-					
-					const popupSettings: MPopupSettings = response.data;
-					//console.log(popupSettings);
-					
-					let descriptionColor: string    = popupSettings.description.text_color ? `color:${ popupSettings.description.text_color };` : '',
-					    descriptionFontSize: string = popupSettings.description.text_size ? `font-size:${ popupSettings.description.text_size };` : '',
-					    descriptionAlign: string    = popupSettings.description.text_align ? `text-align:${ popupSettings.description.text_align };` : '',
-					    descriptionPadding: string  = popupSettings.description.padding ? `padding:${ popupSettings.description.padding };` : '',
-					    description: string         = `<p data-transient-key="${ popupSettings.transient_key }" style="${ descriptionColor + descriptionFontSize + descriptionAlign + descriptionPadding }">${ popupSettings.description.text }</p>`,
-					    versionColor: string        = '',
-					    versionBackground: string   = '',
-					    version: string             = '',
-					    titleColor: string          = popupSettings.title.text_color ? `color:${ popupSettings.title.text_color };` : '',
-					    titleFontSize: string       = popupSettings.title.text_size ? `font-size:${ popupSettings.title.text_size };` : '',
-					    titleAlign: string          = popupSettings.title.text_align ? `text-align:${ popupSettings.title.text_align };` : '',
-					    title: string,
-					    buttons: string             = '',
-					    hoverButtons                = popupSettings.hoverButtons || '',
-					    imageTopLeft: string        = popupSettings.images.top_left,
-					    logo: string                = `<img class="mp-logo" src="${ popupSettings.images.logo }">`,
-					    footerNoticeStyle: string   = popupSettings.footerNotice.bg_color ? ` style="background-color:${ popupSettings.footerNotice.bg_color };"` : '',
-						footerNotice: string        = popupSettings.footerNotice.text ? `<div class="footer-notice"${ footerNoticeStyle }>${ popupSettings.footerNotice.text }</div>` : '';
-					
-					if (popupSettings.version && Object.keys(popupSettings.version).length) {
-						versionColor        = popupSettings.version.text_color ? `color:${ popupSettings.version.text_color };` : '';
-						versionBackground   = popupSettings.version.background ? `background:${ popupSettings.version.background };` : '';
-						version             = `<span class="version" style="${ versionBackground + versionColor }">${ popupSettings.version.text }</span>`;
+			success : ( response: any ) => {
+
+				if ( response.success === true ) {
+
+					const popupSettings: MPopupSettings = response.data,
+					      descriptionColor: string      = popupSettings.description.text_color ? `color:${ popupSettings.description.text_color };` : '',
+					      descriptionFontSize: string   = popupSettings.description.text_size ? `font-size:${ popupSettings.description.text_size };` : '',
+					      descriptionAlign: string      = popupSettings.description.text_align ? `text-align:${ popupSettings.description.text_align };` : '',
+					      descriptionPadding: string    = popupSettings.description.padding ? `padding:${ popupSettings.description.padding };` : '',
+					      description: string           = `<p data-transient-key="${ popupSettings.transient_key }" style="${ descriptionColor + descriptionFontSize + descriptionAlign + descriptionPadding }">${ popupSettings.description.text }</p>`,
+					      titleColor: string            = popupSettings.title.text_color ? `color:${ popupSettings.title.text_color };` : '',
+					      titleFontSize: string         = popupSettings.title.text_size ? `font-size:${ popupSettings.title.text_size };` : '',
+					      titleAlign: string            = popupSettings.title.text_align ? `text-align:${ popupSettings.title.text_align };` : '',
+					      hoverButtons                  = popupSettings.hoverButtons || '',
+					      imageTopLeft: string          = popupSettings.images.top_left,
+					      footerNoticeStyle: string     = popupSettings.footerNotice.bg_color ? ` style="background-color:${ popupSettings.footerNotice.bg_color };"` : '',
+					      footerNotice: string          = popupSettings.footerNotice.text ? `<div class="footer-notice"${ footerNoticeStyle }>${ popupSettings.footerNotice.text }</div>` : '';
+
+					let logo: string              = `<img class="mp-logo" src="${ popupSettings.images.logo }">`,
+					    versionColor: string      = '',
+					    versionBackground: string = '',
+					    version: string           = '',
+					    buttons: string           = '';
+
+					if ( popupSettings.images.hasOwnProperty( 'logo_css' ) && popupSettings.images.logo_css ) {
+						logo = logo.replace( '>', ` style="${ popupSettings.images.logo_css }">` );
+					}
+
+					if ( popupSettings.version && Object.keys( popupSettings.version ).length ) {
+						versionColor = popupSettings.version.text_color ? `color:${ popupSettings.version.text_color };` : '';
+						versionBackground = popupSettings.version.background ? `background:${ popupSettings.version.background };` : '';
+						version = `<span class="version" style="${ versionBackground + versionColor }">${ popupSettings.version.text }</span>`;
 					}
 					
-					title = `<h1 style="${ titleColor + titleFontSize + titleAlign }"><span>${ popupSettings.title.text + version }</span></h1>`
+					const title: string = popupSettings.title.text ? `<h1 style="${ titleColor + titleFontSize + titleAlign }"><span>${ popupSettings.title.text + version }</span></h1>` : '';
 					
 					// Add buttons.
 					if ( popupSettings.buttons && popupSettings.buttons.length ) {
-						if ( hoverButtons ) {
 
+						if ( hoverButtons ) {
 							$( hoverButtons ).appendTo( 'body' );
 						}
+
 						popupSettings.buttons.forEach( ( button: MPopupButton ) => {
 							buttons += `<button data-url="${ button.url }" class="${ button.class } popup-button" style="${ button.css }">${ button.text }</button>`;
 						} );
+
 					}
-					
-					Swal.fire({
-						width             : 520,
-						padding           : null,
-						customClass       : {
-							popup: 'marketing-popup'
+
+					Swal.fire( {
+						width            : 520,
+						padding          : null,
+						customClass      : {
+							popup: 'marketing-popup',
 						},
-						background        : popupSettings.background,
-						showCloseButton   : true,
-						showConfirmButton : false,
-						html              : logo + title + description + buttons + footerNotice,
-						imageUrl          : imageTopLeft,
-					});
-					
-					// Redirect to button url
-					$('.popup-button').on('click', (evt: JQueryEventObject) => {
+						background       : popupSettings.background,
+						showCloseButton  : true,
+						showConfirmButton: false,
+						html             : logo + title + description + buttons + footerNotice,
+						imageUrl         : imageTopLeft,
+					} );
+
+					// Redirect to button url.
+					$( '.popup-button' ).click( ( evt: JQueryEventObject ) => {
 						evt.preventDefault();
-						window.open( $(evt.currentTarget).data('url'), '_blank' );
-					});
-					
-					// Hide popup when click un close button for this user
-					$('.marketing-popup .swal2-close').on('click',() => {
-						this.hideMarketingPopup( $('.swal2-content p').data('transient-key') );
-					});
+						window.open( $( evt.currentTarget ).data( 'url' ), '_blank' );
+					} );
+
+					// Hide popup when click un close button for this user.
+					$( '.marketing-popup .swal2-close' ).click( () => {
+						this.hideMarketingPopup( $( '.swal2-content p' ).data( 'transient-key' ) );
+					} );
 					
 				}
 			},
 		});
 		
 	}
-	
-	hideMarketingPopup(transientKey: string) {
-		
-		$.ajax({
-			url       : window['ajaxurl'],
-			dataType  : 'json',
-			method    : 'post',
-			data      : {
-				action       : 'atum_hide_marketing_popup',
-				token        : this.settings.get('nonce'),
-				transientKey : transientKey,
+
+	hideMarketingPopup( transientKey: string ) {
+
+		$.ajax( {
+			url     : window[ 'ajaxurl' ],
+			dataType: 'json',
+			method  : 'post',
+			data    : {
+				action      : 'atum_hide_marketing_popup',
+				token       : this.settings.get( 'nonce' ),
+				transientKey: transientKey,
 			},
-		});
-		
+		} );
+
 	}
 	
 }
