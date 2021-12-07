8de3f10280114cd81f99b21b961f2571f4cdfa74
Salva Machí <salvamb@berebel.io>
2021-12-07 11:19:14 +0100

    Fixed PHP 8.1 compatibility code

--- classes/Inc/Helpers.php
+++ classes/Inc/Helpers.php
@@ -2454,17 +2454,6 @@ final class Helpers {
 			$grouped[ $key ][] = $value;
 		}
 
-		// Recursively build a nested grouping if more parameters are supplied.
-		// Each grouped array value is grouped according to the next sequential key.
-		if ( func_num_args() > 2 ) {
-			$args = func_get_args();
-
-			foreach ( $grouped as $key => $value ) {
-				$params          = array_merge( [ $value ], array_slice( $args, 2, func_num_args() ) );
-				$grouped[ $key ] = call_user_func_array( 'array_group_by', $params );
-			}
-		}
-
 		return $grouped;
 
 	}
