abf8e80546890b35cf9bae004e51bec78dff2046
Salva Machí <salvamb@berebel.io>
2022-09-05 12:46:49 +0200

    Added new helper to convert PHP date formats to moment.js formats

--- classes/Inc/Helpers.php
+++ classes/Inc/Helpers.php
@@ -3792,4 +3792,65 @@ final class Helpers {
 
 	}
 
+	/**
+	 * Convert a PHP date format to a compatible moment.js date format
+	 *
+	 * @since 1.9.21
+	 *
+	 * @param string $php_date_format
+	 *
+	 * @return string
+	 */
+	public static function convert_php_date_format_to_moment( $php_date_format ) {
+
+		$replacements = [
+			'A' => 'A',      // for the sake of escaping below.
+			'a' => 'a',      // for the sake of escaping below.
+			'B' => '',       // Swatch internet time (.beats), no equivalent.
+			'c' => 'YYYY-MM-DD[T]HH:mm:ssZ', // ISO 8601.
+			'D' => 'ddd',
+			'd' => 'DD',
+			'e' => 'zz',     // deprecated since version 1.6.0 of moment.js.
+			'F' => 'MMMM',
+			'G' => 'H',
+			'g' => 'h',
+			'H' => 'HH',
+			'h' => 'hh',
+			'I' => '',       // Daylight Saving Time? => moment().isDST().
+			'i' => 'mm',
+			'j' => 'D',
+			'L' => '',       // Leap year? => moment().isLeapYear().
+			'l' => 'dddd',
+			'M' => 'MMM',
+			'm' => 'MM',
+			'N' => 'E',
+			'n' => 'M',
+			'O' => 'ZZ',
+			'o' => 'YYYY',
+			'P' => 'Z',
+			'r' => 'ddd, DD MMM YYYY HH:mm:ss ZZ', // RFC 2822.
+			'S' => 'o',
+			's' => 'ss',
+			'T' => 'z',      // deprecated since version 1.6.0 of moment.js.
+			't' => '',       // days in the month => moment().daysInMonth().
+			'U' => 'X',
+			'u' => 'SSSSSS', // microseconds.
+			'v' => 'SSS',    // milliseconds (from PHP 7.0.0).
+			'W' => 'W',      // for the sake of escaping below.
+			'w' => 'e',
+			'Y' => 'YYYY',
+			'y' => 'YY',
+			'Z' => '',       // time zone offset in minutes => moment().zone();.
+			'z' => 'DDD',
+		];
+
+		// Converts escaped characters.
+		foreach ( $replacements as $from => $to ) {
+			$replacements[ '\\' . $from ] = '[' . $from . ']';
+		}
+
+		return strtr( $php_date_format, $replacements );
+
+	}
+
 }
