c6bb06bc0e626d3f3aacb966c9a3627000b00ce6
Salva Machí <salvamb@berebel.io>
2023-02-16 09:51:38 +0100

    Clean up wrong ATUM keys

--- atum-stock-manager-for-woocommerce.php
+++ atum-stock-manager-for-woocommerce.php
@@ -10,7 +10,7 @@
  * Plugin Name:          ATUM Inventory Management for WooCommerce
  * Plugin URI:           https://stockmanagementlabs.com/
  * Description:          The ultimate stock management plugin for serious WooCommerce sellers
- * Version:              1.9.26.2
+ * Version:              1.9.27
  * Author:               Stock Management Labs™
  * Author URI:           https://stockmanagementlabs.com/
  * Contributors:         Be Rebel Studio - https://berebel.io
@@ -18,7 +18,7 @@
  * Tested up to:         6.1.1
  * Requires PHP:         7.1
  * WC requires at least: 5.0
- * WC tested up to:      7.3.0
+ * WC tested up to:      7.4.0
  * Text Domain:          atum-stock-manager-for-woocommerce
  * Domain Path:          /languages
  * License:              GPLv2 or later
@@ -28,7 +28,7 @@
 defined( 'ABSPATH' ) || die;
 
 if ( ! defined( 'ATUM_VERSION' ) ) {
-	define( 'ATUM_VERSION', '1.9.26.2' );
+	define( 'ATUM_VERSION', '1.9.27' );
 }
 
 if ( ! defined( 'ATUM_WC_MINIMUM_VERSION' ) ) {
