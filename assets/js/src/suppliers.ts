82c6b809864d0abefd147497860a268f7dc3ad08
José Andrés Piera Arnandis <jose@berebel.studio>
2023-07-19 12:14:59 +0200

    Add flag to suppliers

--- assets/js/src/suppliers.ts
+++ assets/js/src/suppliers.ts
@@ -6,17 +6,29 @@
  * @since 1.9.19
  */
 
+/**
+ * Third Party Plugins
+ */
+
+
+import '../vendor/select2';      // A fixed version compatible with webpack
+
 
 /**
  * Components
  */
 
+import Settings from './config/_settings';
 import Supplier from './components/suppliers/_supplier';
+import EnhancedSelect from './components/_enhanced-select';
 
 // Modules that need to execute when the DOM is ready should go here.
 jQuery( ( $: JQueryStatic ) => {
-	
-	// Get the options from the localized var.
-	new Supplier();
+
+	// Get the options from the localized var. For Now only will be set if WPML is active.
+	const settings                       = new Settings( 'atumSupplierVars' ),
+	      enhancedSelect: EnhancedSelect = new EnhancedSelect();
+
+	new Supplier( settings, enhancedSelect );
 
 });
