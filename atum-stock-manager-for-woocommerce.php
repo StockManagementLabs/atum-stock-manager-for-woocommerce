a3dfc06d12fb104d78ade83520d84362374b4d0c
Salva Machí <salva@berebel.studio>
2024-11-12 11:46:14 +0100

    Version bump

--- atum-stock-manager-for-woocommerce.php
+++ atum-stock-manager-for-woocommerce.php
@@ -11,12 +11,12 @@
  * Requires Plugins:     woocommerce
  * Plugin URI:           https://stockmanagementlabs.com/
  * Description:          The ultimate stock management plugin for serious WooCommerce sellers
- * Version:              1.9.42
+ * Version:              1.9.43.1
  * Author:               Stock Management Labs™
  * Author URI:           https://stockmanagementlabs.com/
  * Contributors:         BE REBEL - https://berebel.studio
  * Requires at least:    5.9
- * Tested up to:         6.6.2
+ * Tested up to:         6.7
  * Requires PHP:         7.3
  * WC requires at least: 5.0
  * WC tested up to:      9.3.3
@@ -29,7 +29,7 @@
 defined( 'ABSPATH' ) || die;
 
 if ( ! defined( 'ATUM_VERSION' ) ) {
-	define( 'ATUM_VERSION', '1.9.42' );
+	define( 'ATUM_VERSION', '1.9.43.1' );
 }
 
 if ( ! defined( 'ATUM_WC_MINIMUM_VERSION' ) ) {
