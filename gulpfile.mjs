a2773b57bf57104aeabf15a9e79abd720664caac
Salva Machí <salva@berebel.studio>
2025-03-24 13:04:22 +0100

    Updated gulpfile

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -18,7 +18,7 @@ import gulpSass from 'gulp-sass';
 const sass = gulpSass( dartSass );
 
 // Plugin version
-const version = '1.9.45',
+const version = '1.9.46',
       curDate = new Date();
 
 // Global config
